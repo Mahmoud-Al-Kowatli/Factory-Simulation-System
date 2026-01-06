73633601435bb3c0fe8ef6fac54e4e6be6beccf2
Ghaith Salloum <ghaithsalloum12@gmail.com>
2026-01-06 21:32:28 +0300

    Added User.h

--- FactoryManager/User.h
+++ FactoryManager/User.h
@@ -1,1 +1,29 @@
 #pragma once
+#include"Libraries.h"
+
+class MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+
+	void login() {
+
+	}
+
+	void logout() {
+
+	}
+
+private:
+	string username;
+	string password;
+};
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
