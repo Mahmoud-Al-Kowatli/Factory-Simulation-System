da6a1890e276a3657d0dba88843f1424658a7988
Ghaith Salloum <ghaithsalloum12@gmail.com>
2026-01-06 21:13:41 +0300

    TEST

---
+++ FactoryManager/Libraries.h
@@ -0,0 +1,23 @@
+#pragma once
+
+//all Libraries needed in the project.
+
+#include<iostream>
+#include<string>
+#include<iomanip>
+
+#include<cmath>
+#include<algorithm>
+
+#include<vector>
+#include<list>
+#include<queue>
+#include<unordered_map>
+#include<deque>
+#include<stack>
+#include<map>
+
+#include<ctime>
+
+// so no1 forgets to add it
+using namespace std;
