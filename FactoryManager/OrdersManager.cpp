ce3f85edc9f5c57e73725bac6926c1f62b45a627
Ghaith Salloum <ghaithsalloum12@gmail.com>
2026-01-08 20:08:54 +0300

    Added some comments for simplicity

--- FactoryManager/OrdersManager.cpp
+++ FactoryManager/OrdersManager.cpp
@@ -10,16 +10,20 @@ void OrdersManager::receiveOrder(Order order)
 
 	list<Order>::iterator insertedIt;
 
+	//if Urgent it adds it at the beggining of the list
 	if (order.getPriority() == Order::Priority::URGENT)
 	{
 		receptionQueue.push_front(order);
 		insertedIt = receptionQueue.begin();
 	}
+	//if Normal it adds it at the end of the list
 	else if (order.getPriority() == Order::Priority::NORMAL)
 	{
 		receptionQueue.push_back(order);
 		insertedIt = --receptionQueue.end();
 	}
+
+	//if VIP it adds it after the last Urgent "basically when it finds the first Normal Order it inserts it after it"
 	else
 	{
 
