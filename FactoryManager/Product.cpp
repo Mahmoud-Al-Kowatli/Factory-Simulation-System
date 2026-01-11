51254b720856db9d52d47074443edf4f3322c9c7
MajedAnka <majedanka444@gmail.com>
2026-01-11 23:13:28 +0200

    my branch

--- FactoryManager/Product.cpp
+++ FactoryManager/Product.cpp
@@ -4,15 +4,15 @@ int Product::counter = 0;
 
 unordered_map<int, Product> Product::productDatabase = {};
 
-const Product Product::OFFICER_CHAIR = Product("Officer Chair", 100, { {Material::FABRIC, 2}, {Material::SCREW, 12}, {Material::METAL_FRAME, 1} });
+const Product Product::OFFICER_CHAIR = Product("Officer Chair", 100, {{Material::FABRIC, 2}, {Material::SCREW, 12}, {Material::METAL_FRAME, 1}});
 
-const Product Product::WOODEN_DESK = Product("Wooden Desk", 80, { {Material::WOOD_PANELS, 3}, {Material::SCREW, 20}, {Material::METAL_FRAME, 1}, { Material::PAINT, 1 } });
+const Product Product::WOODEN_DESK = Product("Wooden Desk", 80, {{Material::WOOD_PANELS, 3}, {Material::SCREW, 20}, {Material::METAL_FRAME, 1}, {Material::PAINT, 1}});
 
-const Product Product::GAMING_DESK = Product("Gaming Desk", 200, { { Material::WOOD_PANELS, 2 }, { Material::SCREW, 15 }, { Material::LED_UNITS, 2 }, { Material::METAL_FRAME, 1 } });
+const Product Product::GAMING_DESK = Product("Gaming Desk", 200, {{Material::WOOD_PANELS, 2}, {Material::SCREW, 15}, {Material::LED_UNITS, 2}, {Material::METAL_FRAME, 1}});
 
-const Product Product::BOOKSHELF = Product("Bookshelf", 70, { { Material::WOOD_PANELS, 5 }, { Material::SCREW, 24 }, { Material::METAL_FRAME, 1 }, { Material::PAINT, 1 } });
+const Product Product::BOOKSHELF = Product("Bookshelf", 70, {{Material::WOOD_PANELS, 5}, {Material::SCREW, 24}, {Material::METAL_FRAME, 1}, {Material::PAINT, 1}});
 
-const Product Product::DRAWER_CABINET = Product("Drawer Cabient", 150, { { Material::WOOD_PANELS, 4 }, { Material::SCREW, 16 }, { Material::METAL_HANDLES, 4 }, { Material::PAINT, 2 }, { Material::METAL_FRAME, 1 } });
+const Product Product::DRAWER_CABINET = Product("Drawer Cabient", 150, {{Material::WOOD_PANELS, 4}, {Material::SCREW, 16}, {Material::METAL_HANDLES, 4}, {Material::PAINT, 2}, {Material::METAL_FRAME, 1}});
 
 Product::Product() : id(0), name(""), unitPrice(0) {}
 
@@ -22,7 +22,8 @@ Product::Product(string name, double unitPrice, vector<pair<Material, int>> requ
 	this->name = name;
 	this->unitPrice = unitPrice;
 	this->requirements = requirements;
-	auto atd = [=](const Product& product) {
+	auto atd = [=](const Product &product)
+	{
 		productDatabase[product.getID()] = product;
 	};
 	atd(BOOKSHELF);
@@ -51,4 +52,3 @@ Product Product::getProduct(int id)
 {
 	return productDatabase[id];
 }
-
